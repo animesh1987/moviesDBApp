9662fff5bf7f51a51128ae8677250efb8c6e5075
Animesh Singh <animeshsingh@google.com>
2018-05-30 16:56:45 +1000

    start redux setup

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import * as ReactDom from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './main.scss';
+
+console.log(Provider, createStore);
 
 import { App } from './app';
 
